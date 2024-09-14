57d79b5e6f74885ba1e3558a5d3257df3a174f60
Emiliano <emiliano@Emilianos-MacBook-Pro.local>
2024-09-14 18:24:47 -0300

    refactor hook

--- app/src/api/payments.ts
+++ app/src/api/payments.ts
@@ -1,12 +1,12 @@
 import { Payment } from "../types/payments";
 
 const data: Payment[] = [
-  { id: "1", date: "09-01-2024", description: "test - 1", amount: 100 },
-  { id: "2", date: "09-02-2024", description: "test - 2", amount: 110 },
-  { id: "3", date: "09-03-2024", description: "test - 3", amount: 120 },
-  { id: "4", date: "09-04-2024", description: "test - 4", amount: 130 },
-  { id: "5", date: "09-05-2024", description: "test - 5", amount: 140 },
-  { id: "6", date: "09-06-2024", description: "test - 6", amount: 150 },
+  { id: "1", date: "09-11-2024", description: "test - 1", amount: 100 },
+  { id: "2", date: "09-12-2024", description: "test - 2", amount: 110 },
+  { id: "3", date: "09-13-2024", description: "test - 3", amount: 120 },
+  { id: "4", date: "09-14-2024", description: "test - 4", amount: 130 },
+  { id: "5", date: "09-15-2024", description: "test - 5", amount: 140 },
+  { id: "6", date: "09-16-2024", description: "test - 6", amount: 150 },
 ];
 
 export const getPayments = async (): Promise<Payment[]> => {
